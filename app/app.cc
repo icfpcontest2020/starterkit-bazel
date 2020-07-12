35a35ae728077f91351b29219da7262e3a00607d
Alexey Kirpichnikov <alex.kirp@gmail.com>
2020-07-12 12:49:06 +0500

    Better error processing

--- app/app.cc
+++ app/app.cc
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
                          cpr::Body{argv[2]},
                          cpr::Header{{"Content-Type", "text/plain"}});
     if (res.status_code != 200) {
-        std::cout << "Unexpected server response: " << res.text << std::endl;
+        std::cout << "Unexpected server response:\nHTTP code: " << res.status_code << "\nResponse body: " << res.text << std::endl;
         return 2;
     }
     std::cout << "Server response: " << res.text << std::endl;
